3c84015211e721b56f679ba63bcdb8c0773546d7
Li Haotian <2878348186@qq.com>
2022-07-08 20:55:48 +0800

    Create 目录的常用管理操作.py

---
+++ 文件/目录的常用管理操作.py
@@ -0,0 +1,14 @@
+# 文件操作
+'''
+重命名文件  os.rename(源文件名，目标文件名)
+删除文件    os.remove(文件名)
+'''
+# 目录操作
+'''
+目录列表    os.listdir(目录名)
+创建目录    os.mkdir(目录名)
+删除目录    os.rmdir(目录名)
+获取当前目录    os.getcwd()
+修改工作目录    os.chdir(目标目录)
+判断是否是文件    os.path.isdir(文件路径)
+'''
